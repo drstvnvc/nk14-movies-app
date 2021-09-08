e34b1d5ddf278c85e48a7735e86721d4969e33d2
Drago Stevanovic <drago@sounder.fm>
2021-09-08 22:02:06 +0200

    Implement sorting

--- src/containers/AppMovies.js
+++ src/containers/AppMovies.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import MovieRow from "../components/MovieRow";
@@ -8,20 +8,52 @@ export default function AppMovies() {
   const movies = useSelector(selectMovies);
   const dispatch = useDispatch();
 
+  const [sortCriteria, setSortCriteria] = useState("id");
+  const [sortDirection, setSortDirection] = useState(1);
+
+  const sortedMovies = [...movies.data].sort((m1, m2) => {
+    if (m1[sortCriteria] < m2[sortCriteria]) {
+      return -1 * sortDirection;
+    }
+    if (m1[sortCriteria] > m2[sortCriteria]) {
+      return 1 * sortDirection;
+    }
+    return 0;
+  });
   useEffect(() => {
     dispatch(getMovies());
   }, []);
 
+  function sortBy(criteria) {
+    // change sort criteria
+    setSortCriteria(criteria);
+
+    // if sort criteria is not changed
+    if (criteria == sortCriteria) {
+      // change only direction
+      setSortDirection(-1 * sortDirection);
+    } else {
+      // if sort criteria is changed
+      // set ascending order
+      setSortDirection(1);
+    }
+  }
   return (
     <div>
       <h1>App movies</h1>
 
-      {movies.data.length ? (
-        <ul>
-          {movies.data.map((movie) => (
-            <MovieRow key={movie.id} movie={movie} />
-          ))}
-        </ul>
+      {sortedMovies.length ? (
+        <div>
+          <div>
+            <span onClick={() => sortBy("title")}>Title</span>
+            <span onClick={() => sortBy("director")}>Director</span>
+          </div>
+          <ul>
+            {sortedMovies.map((movie) => (
+              <MovieRow key={movie.id} movie={movie} />
+            ))}
+          </ul>
+        </div>
       ) : (
         <div>Oops! There are no movies with this title</div>
       )}
